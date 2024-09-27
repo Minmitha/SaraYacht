998feed727024b1e61301dc825bde710cb84bbe9
Minmitha <minmitha@cloudrevelinnovation.com>
2024-09-27 18:27:31 +0530

    added button

---
+++ src/Components/Button.js
@@ -0,0 +1,11 @@
+export function Button() {
+    return (
+        <>
+            <button class="relative px-12 py-4 text-[15px] font-bold  text-white bg-[#082A3A] border-[0.25em] border-[#0F7D85] rounded-full outline-none transition-all duration-300 hover:shadow-[0_0_1em_0.25em_#0F7D85,0_0_4em_1em_rgba(15,125,133,0.781),inset_0_0_0.75em_0.25em_#0F7D85] hover:text-shadow-[0_0_0.5em_#0F7D85]">
+                Explore Now
+            </button>
+
+
+        </>
+    )
+}
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,27 @@
+import { Button } from "./Button";
 import { Navbar } from "./Navbar";
 
 export function Home() {
     return (
         <>
-            <div className="h-screen bg-blue-500">
+            <div className="h-screen bg-[#022447]">
                 <Navbar />
-                <div>
-                    <img src="./images/Rectangle.webp" className="h-screen w-full"/>
+                <div className="h-full w-full">
+                    <div className="relative h-screen">
+                        <img src="./images/Rectangle.webp" className="h-full w-full object-cover" alt="Yacht Background" />
+                        <div className="absolute inset-0 flex flex-col justify-center items-center text-center text-white px-4">
+                            <p className="text-4xl font-bold md:text-6xl">Discover the Ultimate Yacht Experience</p>
+                            <p className="text-lg md:text-5xl mt-4">Book your dream yacht adventure today!</p>
+                            <p className="text-lg md:text-5xl mt-2">Luxury, Comfort, and Unmatched Sailing</p>
+                            <div className="mt-6"> 
+                                <Button /> 
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
+
+
             </div>
         </>
     )
